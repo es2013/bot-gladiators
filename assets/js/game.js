23676457d5f90f1577159bbc40b533dd1621e32b
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2020-12-21 08:23:48 -0800

    added js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,25 @@
-window.alert("This is an alert! JavaScript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotations marks around playerName
+window.alert(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+
+var name = "your name";
+console.log(name);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+ // fight();
+
+ // Tony the Robot is ready for battle!
+console.log(playerName + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
