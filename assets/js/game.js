21c12829ff56319cf6fd31f8122f2d22b9c58758
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2020-12-21 09:52:07 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,6 +28,9 @@ var enemyNames = ["robocop", "deathdroid", "kyle"];
 //   }
 
 var fight = function(enemyName) {
+    // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP'");
@@ -65,8 +68,11 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
     }
      
-  };
+  }
+}
 for(var i = 0; i<enemyNames.length; i++){
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth=50;
     fight(enemyNames[i]);
 }
 
